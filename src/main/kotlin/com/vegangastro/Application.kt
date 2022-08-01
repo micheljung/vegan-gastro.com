c1fa94bea62d2e8f83d9fbc580fd696e08808401
Michel Jung <michel.jung89@gmail.com>
2022-08-01 15:12:02 +0200

    Add resources to auto reload
    However, auto reload is broken by Koin

--- src/main/kotlin/com/vegangastro/Application.kt
+++ src/main/kotlin/com/vegangastro/Application.kt
@@ -17,8 +17,6 @@ import io.ktor.server.webjars.*
 import okhttp3.OkHttpClient
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.StdOutSqlLogger
-import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.koin.core.KoinApplication
 import org.koin.core.annotation.ComponentScan
@@ -28,11 +26,12 @@ import org.koin.core.context.GlobalContext.startKoin
 import org.koin.ksp.generated.module
 
 fun main() {
-  embeddedServer(Netty, port = 8080, host = "0.0.0.0", watchPaths = listOf("classes")) {
+  embeddedServer(Netty, port = 8080, host = "0.0.0.0", watchPaths = listOf("classes", "resources")) {
     install(Webjars)
     val koin = startKoin {
-      printLogger()
       modules(AppModule().module)
+      // Needed for auto reload to work
+      properties(mapOf("application" to this))
     }
     initDatabase(koin)
     configureRouting()
@@ -48,7 +47,7 @@ private fun initDatabase(koin: KoinApplication) {
   )
 
   transaction {
-    addLogger(StdOutSqlLogger)
+//    addLogger(StdOutSqlLogger)
     SchemaUtils.create(PlaceTable)
   }
 }
