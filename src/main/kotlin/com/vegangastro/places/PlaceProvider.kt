44d6dbb7c79468980d85bda9cf50674d6f7734ff
Michel Jung <michel.jung89@gmail.com>
2022-08-01 15:15:17 +0200

    Reduce sleeping time to max 2s

--- src/main/kotlin/com/vegangastro/places/PlaceProvider.kt
+++ src/main/kotlin/com/vegangastro/places/PlaceProvider.kt
@@ -3,26 +3,45 @@ package com.vegangastro.places
 import com.google.maps.GeoApiContext
 import com.google.maps.PlacesApi
 import com.google.maps.model.PlaceType
+import com.google.maps.model.PlacesSearchResponse
 import org.koin.core.annotation.Single
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import org.slf4j.LoggerFactory
+import java.time.Duration
+import java.time.Instant
+import java.util.concurrent.atomic.AtomicReference
 import java.util.stream.Stream
 
 @Single
 class PlaceProvider : KoinComponent {
+
+  private val logger = LoggerFactory.getLogger(javaClass)
   private val geoApiContext by inject<GeoApiContext>()
   private val placeRepository by inject<PlaceRepository>()
 
   fun getPlaces(country: String): Stream<Place> {
+    logger.info("Fetching from Places API")
     val initialResponse = PlacesApi.textSearchQuery(geoApiContext, PlaceType.RESTAURANT)
       .region(country)
       .await()
 
+    val lastQueryTime = AtomicReference(Instant.now())
+
     return Stream
       .iterate(
         initialResponse,
-        { t -> t.nextPageToken != null },
-        { t -> Thread.sleep(2000); PlacesApi.textSearchNextPage(geoApiContext, t.nextPageToken).await() },
+        { t -> t.results.isNotEmpty() },
+        { t ->
+          if (t.nextPageToken == null) PlacesSearchResponse().apply { results = emptyArray() }
+          else {
+            val sleepTime = Duration.between(Instant.now(), lastQueryTime.get().plusSeconds(2)).toMillis().coerceAtLeast(0)
+            Thread.sleep(sleepTime)
+            lastQueryTime.set(Instant.now())
+            logger.info("Fetching next page from Places API")
+            PlacesApi.textSearchNextPage(geoApiContext, t.nextPageToken).await()
+          }
+        },
       )
       .flatMap { Stream.of(*it.results) }
       .map { getPlaceDetails(it.placeId) }
